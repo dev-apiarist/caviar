991eb8021192e2c4c6f6ba8e0c2bc14a19ae7b1e
Kenni Leslie <klesliedev@gmailcom>
2022-04-10 12:34:34 -0500

    60%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/header/nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeroComponent } from './components/hero/hero.component';
+import { ButtonComponent } from './components/button/button.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BuzzComponent } from './components/buzz/buzz.component';
+import { MissionComponent } from './components/mission/mission.component';
+import { NewsletterComponent } from './components/newsletter/newsletter.component';
+import { SectionComponent } from './components/section/section.component';
+
+// material >
+// import { MatButtonModule } from '@angular/material/button';
+// >
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    HeroComponent,
+    ButtonComponent,
+    FooterComponent,
+    BuzzComponent,
+    MissionComponent,
+    NewsletterComponent,
+    SectionComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/buzz/buzz.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buzz',
+  templateUrl: './buzz.component.html',
+  styleUrls: ['./buzz.component.scss'],
+})
+export class BuzzComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
